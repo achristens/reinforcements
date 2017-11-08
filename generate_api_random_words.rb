d6827440dbc47739902b510e03ae30c5bc2312e1
Abby Christens <achristens@gmail.com>
2017-11-08 15:10:13 -0500

    Generate 10 random words

---
+++ generate_api_random_words.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+list_of_words = []
+
+word = ''
+10.times do
+  word = HTTParty.get('http://setgetgo.com/randomword/get.php')
+  word = word.parsed_response
+  list_of_words << word
+end
+
+puts list_of_words
